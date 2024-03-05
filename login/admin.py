7effdfa8846b70185f71384236d9920d4e510b5b
zekeriya_ozurganci <zekeriyaouzrganci@gmail.com>
2024-03-05 18:48:01 +0300

    register page aded and user table created

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import users
 # Register your models here.
+admin.site.register(users)
+
+def __str__(self):
+    return self.username.value
--- login/models.py
+++ login/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class users(models.Model):
+    username = models.CharField(max_length=20,verbose_name="username")
+    password = models.CharField(max_length=20,verbose_name="password")
+    is_superuser = models.BooleanField(default=False,verbose_name="permissions")
+
+def __str__(self):
+    return self.username
